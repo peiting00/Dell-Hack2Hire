babfd9332dc176d843566d4e5f30674c00838637
Peiting Chew <i19017904@student.newinti.edu.my>
2021-11-09 14:11:32 +0800

    update

---
+++ CRUD/crud.php
@@ -0,0 +1,8 @@
+<header>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+  />
+  <link rel="stylesheet" href="style.css" />
+  <script src="https://unpkg.com/vue/dist/vue.min.js"></script>
+</header>

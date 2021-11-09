166e4a920caf1a0c8e050e32e177d82af198e5de
Peiting Chew <i19017904@student.newinti.edu.my>
2021-11-09 11:54:32 +0800

    database table & connection

---
+++ createTable.php
@@ -0,0 +1,27 @@
+<?php
+
+include "dbConnection.php";
+
+// Create table user if not exists
+$userSql = "CREATE TABLE IF NOT EXISTS user (
+    userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userName VARCHAR(100) NOT NULL UNIQUE, 
+    password CHAR(60) NOT NULL)";
+
+// Create table profile if not exists
+$profileSql= "CREATE TABLE IF NOT EXISTS product(
+    productID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    productName VARCHAR(100) NOT NULL,
+    price INT NOT NULL,
+    quanitty INT NOT NULL)";
+
+if (mysqli_query($conn, $userSql) && mysqli_query($conn, $profileSql)) {
+    header("Location:login.php");
+    exit;
+} else {
+    echo "Failed to create table: ".mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
